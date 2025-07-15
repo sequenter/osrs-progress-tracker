cd84a8a07a791a5e8e2f79c3c42e0dfdf5a33e3f
sequenter <35529212+sequenter@users.noreply.github.com>
2025-07-16 00:00:04 +0100

    Added gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,12 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({ fallback: '404.html' }),
     alias: {
       $assets: './src/assets/'
+    },
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/osrs-progress-tracker' : ''
     }
   }
 };
